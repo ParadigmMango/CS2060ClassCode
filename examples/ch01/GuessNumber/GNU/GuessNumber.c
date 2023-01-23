a9eb0f70ade4d224fd113e300a58fa4f2a3d9d40
ParadigmMango <jacobjhartt@gmail.com>
2023-01-22 22:08:59 -0700

    Adding notes and questions to GuessNumber.c

--- examples/ch01/GuessNumber/GNU/GuessNumber.c
+++ examples/ch01/GuessNumber/GNU/GuessNumber.c
@@ -1,28 +1,37 @@
 // Randomly generate numbers between 1 and 1000 for user to guess.
+// Lack of whitespace here detracts from the overall presentation of the code
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+// Why are these here? These are redundant, and should be in a header file.
 void guessGame(void); // function prototype
 int isCorrect(int, int); // function prototype
 
+// Void arguments in C are best practice to indicate no arguments used
 int main(void)
 {
-   // srand( time( 0 ) ); // seed random number generator
+   // srand( time( 0 ) ); // seed random number generator. Without this, the program generate the same answers when run.
+   // Below are two lines with inconsistent whitespace
     //update with your name
     puts("Hello Jacob");
    guessGame();
+   // Below is random, empty whitespace
 
+// Comments at the end of large functions is best practice
 } // end main
 
+// The comments at the beginning of each non-main method help explain the purpose of each method and is best practice.
 // guessGame generates numbers between 1 and 1000
 // and checks user's guess
 void guessGame(void)
 {
+   // Comments after each variable help explain their purpose.
    int answer; // randomly generated number
    int guess; // user's guess
    int response; // 1 or 2 response to continue game
 
+   // Do while loop ensures at least one round of the game is run.
    // loop until user types 2 to quit game
    do {
       // generate random number between 1 and 1000
@@ -68,7 +77,7 @@ int isCorrect(int g, int a)
 } // end function isCorrect
 
 
-
+// Copyright notices are generally place at the top of a file.
 /**************************************************************************
  * (C) Copyright 1992-2012 by Deitel & Associates, Inc. and               *
  * Pearson Education, Inc. All Rights Reserved.                           *
