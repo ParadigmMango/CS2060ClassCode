56afa139a904d670976c2a1360a12b345201f1e9
ParadigmMango <jacobjhartt@gmail.com>
2023-02-13 22:57:10 -0700

    hw03.c created.

---
+++ examples/homework/hw03.c
@@ -0,0 +1,17 @@
+//!  CS2060.002 Homework Assignment #3
+/*!
+  \file hw03.c
+  \author Jacob Hartt (jacobjhartt@gmail.com)
+  \brief A program which calculates the daily charges of a parking garage.
+  \version 1.0
+  \date 02-16-2023
+ */
+
+
+//! The main function
+/*!
+  \return The exit value
+ */
+int main(void) {
+    return 0;
+}
