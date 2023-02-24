a9308bf58f8b41b3dd3184c002d031ce6c39da51
ParadigmMango <jacobjhartt@gmail.com>
2023-02-23 18:31:28 -0700

    calcAverage() implemented and tested in hw04.c

--- examples/homework/HW04/hw04.c
+++ examples/homework/HW04/hw04.c
@@ -9,11 +9,37 @@
 
 #include <stdio.h>
 
+//! Calculates the average value of an array.
+/*!
+  \param arr the array whose values will be used
+  \param size the size of the array
+  \return The average of the array.
+ */
+double calcAverage(const double arr[], size_t size);
+
 //! The main function.
 /*!
   \return The exit value
  */
 int main(void)
 {
+    double testArr1[] = {1, 2, 3};
+    double testArr2[] = {-1.1, 2.2, 0, 333};
+    double testArr3[] = {0, 0, 2};
+
+    printf("%lf %lf %lf", calcAverage(testArr1, 3), calcAverage(testArr2, 4),
+                          calcAverage(testArr3, 3));
+
     puts("Hello world!");
+}
+
+double calcAverage(const double arr[], size_t size)
+{
+    double sum = 0.0;
+
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+
+    return sum / size;
 }
