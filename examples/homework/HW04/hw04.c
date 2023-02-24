0ca55cde824cf20c2bd3405b43e5a97783e1df04
ParadigmMango <jacobjhartt@gmail.com>
2023-02-23 18:20:22 -0700

    HW04.c created.

---
+++ examples/homework/HW04/hw04.c
@@ -0,0 +1,19 @@
+//!  CS2060.002 Homework Assignment #4
+/*!
+  \file hw04.c
+  \author Jacob Hartt (jacobjhartt@gmail.com)
+  \brief A program that calculates the final grades and class average of a class.
+  \version 1.0
+  \date 02-23-2023
+ */
+
+#include <stdio.h>
+
+//! The main function.
+/*!
+  \return The exit value
+ */
+int main(void)
+{
+    puts("Hello world!");
+}
