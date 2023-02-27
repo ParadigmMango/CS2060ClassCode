c76d69ca3e23c3cdff969fdc31828a8de5e3f288
ParadigmMango <jacobjhartt@gmail.com>
2023-02-27 01:05:07 -0700

    ch07pointerExploration1.c cleaned up

--- examples/ch07/ch07pointerExploration1.c
+++ examples/ch07/ch07pointerExploration1.c
@@ -1,40 +1,48 @@
-/*******************
- Understanding pointers
-
- Add comments to explain the code
-**************/
+//!  Pointers Exploration
+/*!
+  \file ch07pointerExploration1.c
+  \author Jacob Hartt (jacobjhartt@gmail.com)
+  \version 1.0
+  \date 02-27-2023
+ */
 
 #include <stdio.h>
 
 
 int main ()
 {
-	// Initalize houseNum
+	// Initalize the variables and the pointer
 	int houseNum = 13;
 	int calcHouseNum1 = 0;
 	int calcHouseNum2 = 0;
 	int *houseNumPtr = &houseNum;
 	
-	printf("houseNum %d\n", houseNum);
+    // Print out details pertaining to houseNum
+	printf("houseNum = %d\n", houseNum);
+	printf("&houseNum = %p\n", &houseNum);
+    puts("");
 
-	printf("&houseNum %p\n\n", &houseNum);
+    // Print out details pertaining to houseNumPtr
+	printf("*houseNumPtr = %d\n", *houseNumPtr);
+	printf("houseNumPtr = %p\n", houseNumPtr);
+	printf("&houseNumPtr = %p\n", &houseNumPtr);
+    puts("");
 
-	printf ("*houseNumPtr  %d\n", *houseNumPtr);
-
-	printf ("houseNumPtr %p\n\n", houseNumPtr);
-
-	printf ("&houseNumPtr %p\n\n", &houseNumPtr);
-
+    // Demonstrate how dereferenced pointers can still be used for arithmetic.
 	calcHouseNum1 =  *houseNumPtr + *houseNumPtr;
+	printf("*houseNumPtr + *houseNumPtr is %d\n", calcHouseNum1);
+    puts("");
 
-	printf("*houseNumPtr + *houseNumPtr is %d\n\n", calcHouseNum1);
-
+    // Demonstrate how other arithmetic (i.e. multiplication) can be used on
+    // dereferenced pointers.
 	calcHouseNum2 = 2 * (*houseNumPtr);
+	printf("2 * (*houseNumPtr) is %d\n", calcHouseNum2);
+    puts("");
 
-	printf("2 * (*houseNumPtr) is %d\n\n", calcHouseNum2);
-
-	printf("\n&*houseNumPtr = %p"
-		"\n*&houseNumPtr = %p\n", &*houseNumPtr, *&houseNumPtr);
+    // Demonstrate how &* and *& cancel each other out as & and * are opposite
+    // operations.
+	printf("&*houseNumPtr = %p\n", &*houseNumPtr);
+    printf("*&houseNumPtr = %p\n", *&houseNumPtr);
 			
 	return 0;
 }
