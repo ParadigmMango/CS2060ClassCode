7926f40de37fe17685c7fa0b58b8997992f1ebc1
ParadigmMango <jacobjhartt@gmail.com>
2023-03-30 13:49:37 -0600

    structs implemented in Iteration 01

--- examples/homework/Iteration01/iteration01.c
+++ examples/homework/Iteration01/iteration01.c
@@ -9,9 +9,41 @@
 #include <stdio.h>
 #include <time.h>
 
+#define STRING_SIZE 80
+#define TIME_STAMP_SIZE 19
+
+//! An organization struct which packages all relevant information to itself.
+typedef struct organization {
+    // organziation properties
+    char name[STRING_SIZE];
+    char purpose[STRING_SIZE];
+    char url[STRING_SIZE];
+    double goalAmount;
+
+    // owner properties
+    char ownerEmail[STRING_SIZE];
+    char ownerPwd[STRING_SIZE];
+    char ownerFirstName[STRING_SIZE];
+    char ownerLastName[STRING_SIZE];
+
+    // donation tracking
+    unsigned int numDonations;
+    double donationSum;
+
+    // donor tracking
+    unsigned int numDonors;
+} Organization; 
+
+//! An donor struct which packages all relevant information to itself.
+typedef struct donor {
+    char firstName[STRING_SIZE];
+    char fastName[STRING_SIZE];
+    char zip[STRING_SIZE];
+} Donor;
+
 int main(void)
 {
-    char str[19];
+    char str[TIME_STAMP_SIZE];
 
     time_t time_var = time(NULL);
 
